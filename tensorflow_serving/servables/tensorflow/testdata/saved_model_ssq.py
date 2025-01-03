f2ba3b620ec3b415c251ac48caff5ff70198cc60
zailong <1035381826@qq.com>
2025-01-03 18:02:13 +0800

    fix:servable::saved_model_ssq::signatures

--- tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
+++ tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
@@ -12,12 +12,13 @@ class SSQModel(tf.Module):
         self.r1_max = 17
         print("just test")
 
-    def gen_red(self, r1):
+    def gen_red(self, x):
+        r1 = x
         """gen_red on history distribution"""
         if r1 < self.r1_max:
             print("r1 is ok")
         else:
-            r1 = np.random.randint(0, 17)
+            r1 = tf.constant(np.random.randint(0, 17))
         r2 = np.random.randint(r1 + 1, 24)
         r3 = np.random.randint(r2 + 1, 29)
         r4 = np.random.randint(r3 + 1, 31)
@@ -25,12 +26,22 @@ class SSQModel(tf.Module):
         r6 = np.random.randint(r5 + 1, 33)
         return np.array([r1, r2, r3, r4, r5, r6]) + 1
 
-    def predict(self, X):
+    @tf.function(input_signature=[tf.TensorSpec(shape=(), dtype=tf.float32)])
+    def predict(self, x):
         """predict"""
-        return self.gen_red(X)
+
+        return tf.multiply(0.5, x)
+
+    def get_serving_signatures(self):
+        return {
+            tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY: self.predict,
+        }
 
 
 if __name__ == "__main__":
     ssq = SSQModel()
     version = "1"
-    tf.saved_model.save(ssq, f"./saved_model_{ssq.name}/{version}")
+    ssq.predict(7)
+
+    tf.saved_model.save(ssq, f"./saved_model_{ssq.name}/{version}",
+                        signatures=ssq.get_serving_signatures())
