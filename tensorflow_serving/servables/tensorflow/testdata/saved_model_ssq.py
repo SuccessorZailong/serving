6b39287a5f2e53b05be405c2087cd7df6c39288c
zailong <1035381826@qq.com>
2025-01-06 10:49:37 +0800

    refactor:servable::saved_model_ssq::signatures with no py_function

--- tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
+++ tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
@@ -12,24 +12,35 @@ class SSQModel(tf.Module):
         self.r1_max = 17
         print("just test")
 
+    def gen_red(self, r1):
+        """gen_red on history distribution"""
+        r1_max = self.r1_max
+        # Convert inputs to tensors if they are not already
+        r1_max = tf.cast(r1_max, dtype=tf.int32)
+
+        # Define a conditional operation for checking r1 < r1_max
+        def true_fn():
+            return r1
+
+        def false_fn():
+            return tf.random.uniform(shape=[], minval=0, maxval=17, dtype=tf.int32)
+
+        r1 = tf.cond(r1 < r1_max, true_fn, false_fn)
+
+        # Use tf.random.uniform for generating random integers within specified ranges
+        r2 = tf.random.uniform(shape=[], minval=r1 + 1, maxval=24, dtype=tf.int32)
+        r3 = tf.random.uniform(shape=[], minval=r2 + 1, maxval=29, dtype=tf.int32)
+        r4 = tf.random.uniform(shape=[], minval=r3 + 1, maxval=31, dtype=tf.int32)
+        r5 = tf.random.uniform(shape=[], minval=r4 + 1, maxval=32, dtype=tf.int32)
+        r6 = tf.random.uniform(shape=[], minval=r5 + 1, maxval=33, dtype=tf.int32)
+
+        # Stack the results into a single tensor and add 1 to match the original function's behavior
+        return tf.stack([r1, r2, r3, r4, r5, r6]) + 1
+
     @tf.function(input_signature=[tf.TensorSpec(shape=(), dtype=tf.int32)])
     def predict(self, x):
         """predict"""
-
-        def gen_red(r1_max, r1):
-            """gen_red on history distribution"""
-            if r1 < r1_max:
-                print("r1 is ok")
-            else:
-                r1 = tf.constant(np.random.randint(0, 17))
-            r2 = np.random.randint(r1 + 1, 24)
-            r3 = np.random.randint(r2 + 1, 29)
-            r4 = np.random.randint(r3 + 1, 31)
-            r5 = np.random.randint(r4 + 1, 32)
-            r6 = np.random.randint(r5 + 1, 33)
-            return np.array([r1, r2, r3, r4, r5, r6]) + 1
-
-        return tf.py_function(gen_red, [self.r1_max, x], tf.int32)
+        return self.gen_red(x)
 
     def get_serving_signatures(self):
         return {
