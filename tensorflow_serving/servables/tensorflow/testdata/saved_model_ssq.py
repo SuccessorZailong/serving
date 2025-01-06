d684db264b8a0778d8befc1b01312a406d0d0009
zailong <1035381826@qq.com>
2025-01-06 10:11:39 +0800

    refactor:servable::saved_model_ssq::signatures with py_function

--- tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
+++ tensorflow_serving/servables/tensorflow/testdata/saved_model_ssq.py
@@ -12,25 +12,24 @@ class SSQModel(tf.Module):
         self.r1_max = 17
         print("just test")
 
-    def gen_red(self, x):
-        r1 = x
-        """gen_red on history distribution"""
-        if r1 < self.r1_max:
-            print("r1 is ok")
-        else:
-            r1 = tf.constant(np.random.randint(0, 17))
-        r2 = np.random.randint(r1 + 1, 24)
-        r3 = np.random.randint(r2 + 1, 29)
-        r4 = np.random.randint(r3 + 1, 31)
-        r5 = np.random.randint(r4 + 1, 32)
-        r6 = np.random.randint(r5 + 1, 33)
-        return np.array([r1, r2, r3, r4, r5, r6]) + 1
-
-    @tf.function(input_signature=[tf.TensorSpec(shape=(), dtype=tf.float32)])
+    @tf.function(input_signature=[tf.TensorSpec(shape=(), dtype=tf.int32)])
     def predict(self, x):
         """predict"""
 
-        return tf.multiply(0.5, x)
+        def gen_red(r1_max, r1):
+            """gen_red on history distribution"""
+            if r1 < r1_max:
+                print("r1 is ok")
+            else:
+                r1 = tf.constant(np.random.randint(0, 17))
+            r2 = np.random.randint(r1 + 1, 24)
+            r3 = np.random.randint(r2 + 1, 29)
+            r4 = np.random.randint(r3 + 1, 31)
+            r5 = np.random.randint(r4 + 1, 32)
+            r6 = np.random.randint(r5 + 1, 33)
+            return np.array([r1, r2, r3, r4, r5, r6]) + 1
+
+        return tf.py_function(gen_red, [self.r1_max, x], tf.int32)
 
     def get_serving_signatures(self):
         return {
@@ -41,7 +40,8 @@ class SSQModel(tf.Module):
 if __name__ == "__main__":
     ssq = SSQModel()
     version = "1"
-    ssq.predict(7)
+    result = ssq.predict(7)
+    print(result.numpy())
 
     tf.saved_model.save(ssq, f"./saved_model_{ssq.name}/{version}",
                         signatures=ssq.get_serving_signatures())
